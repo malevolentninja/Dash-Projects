bd088a34d5bdcecd9ec217d8de5edf685f71f290
Olivia Boertje <malevolentninja@users.noreply.github.com>
2017-11-29 13:42:25 +0000

    Create main.js
    main.js

---
+++ Html-CSS-Javascript/PROJECT-2-BUILD-A-RESPONSIVE-BLOG-THEME/responsive_blog/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(document).on('click', "#like", function() {
+    alert('Thanks for your like!');
+  });
+  $(document).on('click', "#comment", function() {
+    alert('Thanks for your comment!');
+  });
+});
